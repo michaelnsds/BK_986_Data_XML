b07b073b266ac67b6d6c16e7bea87656c343aa11
michaelnsds <62763445+michaelnsds@users.noreply.github.com>
2023-04-23 04:02:41 -0300

    Instalação de Mod.
    Mod Dressme +Try.

--- L2J_SunriseProject_Data/build/L2SR_DataPack/tools/sql/sunrise_sql_files/sunrise_patch.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `characters` ADD `enchant_bot` INT UNSIGNED DEFAULT 0;
-ALTER TABLE `characters` ADD `enchant_chance` DOUBLE UNSIGNED DEFAULT 80;
-ALTER TABLE `characters` ADD `achievementmobkilled` INT UNSIGNED DEFAULT 0;
-ALTER TABLE `characters` ADD COLUMN `pccafe_points` INT NULL DEFAULT 0 AFTER `createDate`;
